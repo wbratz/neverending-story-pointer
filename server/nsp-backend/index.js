35959c5397589417d446303197eb39de9c404db6
Billy <Billy@williambratz.com>
2021-08-02 22:52:16 -0500

    Messes with some shit

--- server/nsp-backend/index.js
+++ server/nsp-backend/index.js
@@ -1,70 +1,80 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
 
-const io = new Server(server,  {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-})
+  res.sendFile(__dirname + "/index.html");
+});
 
 io.on("connection", (socket) => {
-    const users = [];
-    for (let [id, socket] of io.of("/").sockets) {
-        users.push({
-            userID: id,
-            username: socket.userName,
-            storypoints: 0
-        });
-    }
-    
-    socket.broadcast.emit("user connected", {
-        userID: socket.id,
-        username: socket.userName,
-        storypoints: 0
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.userName,
+      storypoints: socket.storypoints,
     });
+  }
 
-    socket.emit("users", users);
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.userName,
+    storypoints: socket.storypoints,
+  });
 
-    console.log(socket.userName+" connected");
+  socket.emit("users", users);
 
-    socket.on('disconnect', () => {
-        console.log(socket.userName+" disconnected")
+  console.log(socket.userName + " connected");
 
-        socket.broadcast.emit("user disconnected", {
-            userID: socket.id,
-            username: socket.userName,
-        })
+  socket.on("disconnect", () => {
+    console.log(socket.userName + " disconnected");
 
+    socket.broadcast.emit("user disconnected", {
+      userID: socket.id,
+      username: socket.userName,
     });
+  });
 
-    socket.on("submit points", (user) => {
-        socket.broadcast.emit("submit points", user)
-    })
+  socket.on("submit points", (user) => {
+    console.log(users);
 
-    socket.on("user added", (msg) => {
-        io.emit("user added", msg);
-    })
+    let idx = users.findIndex((x) => x.userID === user.userID);
+    console.log(idx);
+
+    users[idx].storypoints = user.storypoints;
+
+    socket.broadcast.emit("submit points", user);
+
+    console.log(user);
+
+    console.log(users);
+  });
+
+  socket.on("user added", (msg) => {
+    io.emit("user added", msg);
+  });
 });
 
 io.use((socket, next) => {
-    
-    const username = socket.handshake.auth.userName;
-   
-    if(!username) {
-        return next(new Error("invalid username"));
-    }
+  const username = socket.handshake.auth.userName;
+  const storypoints = socket.handshake.auth.storypoints;
 
-    socket.userName = username;
-    next();
-})
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.storypoints = storypoints;
+  socket.userName = username;
+  next();
+});
 
 server.listen(3001, () => {
-    console.log("listening on *:3001")
-})
+  console.log("listening on *:3001");
+});
